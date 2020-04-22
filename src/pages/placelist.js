4937fc17374145182ca48546866c616460b0787a
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-04-22 18:18:39 +0900

    Init page for placelist

---
+++ src/pages/placelist.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+import Head from "../components/head";
+
+const PlaceListPage = () => {
+  return (
+    <Layout color="white">
+      <Head />
+    </Layout>
+  );
+};
+
+export default PlaceListPage;
