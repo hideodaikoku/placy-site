efbad36225e6f2659eeb0a674dbbd2f8adfa06e9
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-05-03 05:15:35 +0900

    Build pages correctly

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,14 @@
+const path = require(`path`);
+
 exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions;
+  const placeList = path.resolve(`src/pages/placelist.js`);
   // Only update the `/app` page.
   if (page.path.match(/^\/placelist/)) {
     // page.matchPath is a special key that's used for matching pages
     // with corresponding routes only on the client.
     page.matchPath = "/placelist/*";
+    page.component = placeList;
     // Update the page.
     createPage(page);
   }
--- src/components/placelist/page.js
+++ src/components/placelist/page.js
@@ -3,10 +3,15 @@ import Hero from "./hero";
 import Listings from "./listings";
 import Modal from "./modal";
 import style from "../../styles/placelist.module.scss";
+import SEO from "../seo";
 
 export const Page = () => {
   return (
     <div>
+      <SEO
+        title="Placelist"
+        description="Placy プレイ(ス)リストへようこそ。こちらは、思い入れのある場所のプレイリストを作成して皆と共有するページです。"
+      />
       <Hero />
       <Modal></Modal>
       <hr className={style.spacingBar} />
