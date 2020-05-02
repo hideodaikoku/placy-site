67554f549cdc93f6b62aed5bd7492227181b53b6
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-05-03 05:45:36 +0900

    Try redirecting on netlify

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions;
   const placeList = path.resolve(`src/pages/placelist.js`);
   // Only update the `/app` page.
-  if (page.path.match(/^\/placelist/)) {
+  if (page.path.match(/^\/placelist\/*/)) {
     // page.matchPath is a special key that's used for matching pages
     // with corresponding routes only on the client.
     page.matchPath = "/placelist/*";
