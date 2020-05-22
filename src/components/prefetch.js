9eda781de8ee8696ff0f267299ce394310bf162c
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-05-22 14:35:20 +0900

    crossorigin

--- src/components/prefetch.js
+++ src/components/prefetch.js
@@ -4,7 +4,12 @@ import { Helmet } from "react-helmet";
 export default function Prefetch({ rel, href, probability }) {
   return (
     <Helmet>
-      <link rel={rel} href={href} crossOrigin pr={probability || 0.5}></link>
+      <link
+        rel={rel}
+        href={href}
+        crossOrigin="anonymous"
+        pr={probability || 0.5}
+      ></link>
     </Helmet>
   );
 }
