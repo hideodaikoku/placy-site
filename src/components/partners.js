b3795fe49e424465f0b8b1637eb625bd18fcec8b
Hideo Daikoku <neildaikoku@gmail.com>
2020-05-26 14:32:31 +0900

    fix node

--- src/components/partners.js
+++ src/components/partners.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Img from "gatsby-image";
 import { useStaticQuery, graphql } from "gatsby";
-import {useIntl} from "gasby-plugin-intl";
+import {useIntl} from "gatsby-plugin-intl";
 import partnerStyle from "../styles/partners.module.scss";
 
 const PartnersSection = () => {
