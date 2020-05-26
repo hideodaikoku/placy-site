78b5373f8a5c8b4099e2018797fd6aeb1e7115ec
sp4ghet <8775460+sp4ghet@users.noreply.github.com>
2020-05-26 22:45:05 +0900

    Fix routing error with placelist

--- src/pages/placelist.js
+++ src/pages/placelist.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Router } from "@reach/router";
 
 import { Provider } from "react-redux";
+import { useIntl } from "gatsby-plugin-intl";
 import store from "../components/placelist/redux/store";
 import Layout from "../components/layout";
 import Head from "../components/head";
@@ -9,11 +10,12 @@ import Content from "../components/placelist/page";
 import ListingPage from "../components/placelist/listingPage";
 
 const PlaceListPage = () => {
+  const intl = useIntl();
   return (
     <Layout color="placelist">
       <Head />
       <Provider store={store}>
-        <Router basepath="/placelist">
+        <Router basepath={`/${intl.locale}/placelist`}>
           <Content path="/" />
           <ListingPage path="/:listingId" />
         </Router>
