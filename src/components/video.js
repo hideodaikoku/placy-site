e1817e98153cec71787fff2149a8bee7dae11fc8
Hideo Daikoku <neildaikoku@gmail.com>
2020-09-16 23:05:51 +0900

    add video playback

---
+++ src/components/video.js
@@ -0,0 +1,25 @@
+import React from "react";
+import appStyles from "../styles/app.module.scss";
+
+const Video  = () => {
+    
+    const videoSrcURL = "https://www.youtube.com/embed/0ILP0i2y05U?autoplay=1&loop=1&playlist=0ILP0i2y05U";
+    const videoTitle = "Placy. Find your rhythm";
+    return(
+        <div className={appStyles.introContainer} id={appStyles.videoContainer}>
+                <iframe
+                    src={videoSrcURL}
+                    title={videoTitle}
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    frameBorder="0"
+                    webkitallowfullscreen="true"
+                    mozallowfullscreen="true"
+                    allowFullScreen
+                    mute
+                    style={{height:"75vh", width:"100%", margin:"0px", padding:"0px"}}
+                />
+        </div>
+    )
+}
+
+export default Video;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 import AppSection from "../components/app";
 import PartnersSection from "../components/partners";
+import Video from "../components/video";
 import Top from "../components/top";
 import SEO from "../components/seo";
 
@@ -11,6 +12,7 @@ const IndexPage = () => {
       <SEO />
       <Top />
       <AppSection />
+      <Video />
       <PartnersSection />
     </Layout>
   );
